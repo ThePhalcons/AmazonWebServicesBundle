8df08127240afd6ebd13bdd7f7dd6e0b2796dd5f
jdeveloper <jdeveloper.inxenio@gmail.com>
2014-02-18 12:50:45 +0100

    Added content-encoding header for gzipped files

--- StreamWrapper/S3StreamWrapper.php
+++ StreamWrapper/S3StreamWrapper.php
@@ -30,6 +30,7 @@ class S3StreamWrapper extends BaseS3StreamWrapper
         $response = $this->client($protocol)->create_object($bucket, $object_name, array(
             'body' => $this->buffer,
             'contentType' => $contentType,
+            'headers' => $this->buildHeadersForContent($this->buffer)
         ));
 
         $this->seek_position = 0;
@@ -44,5 +45,35 @@ class S3StreamWrapper extends BaseS3StreamWrapper
         self::$_clients[$protocol] = $s3 ? $s3 : new \AmazonS3();
 
         return stream_wrapper_register($protocol, __CLASS__);
+    }
+
+    private function buildHeadersForContent($content)
+    {
+        $path = tempnam(sys_get_temp_dir(), 's3_mime_type');        
+        file_put_contents($path, $content);
+
+        $headers = array();
+
+        if($this->isFileGzipped($path)){
+            $headers['Content-Encoding'] = "gzip";
+        }
+
+        unlink($path);
+
+        return $headers;
+    }
+
+    private function isFileGzipped($path)
+    {
+        return $this->getFileMimeType($path) == "application/x-gzip";
+    }
+
+    private function getFileMimeType($path)
+    {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimeType = finfo_file($finfo, $path);
+        finfo_close($finfo);
+        
+        return $mimeType;
     }
 }
